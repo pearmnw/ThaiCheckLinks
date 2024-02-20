1c02d4030a34d61d0d24891b2c6deee97e7954bd
Pheeraphat Prisan <77052746+newpheeraphat@users.noreply.github.com>
2024-02-20 19:05:17 +0700

    feat: swap file to pear

--- src/components/loading/Loader.tsx
+++ src/components/loading/Loader.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Loader = () => {
   return (
     <div>
-      <div className='animate-spin rounded-full h-32 w-32 border-b-4 border-blue-500'></div>
+      <div className='animate-spin-slow rounded-full h-32 w-32 border-t-2 border-b-2 border-primary'></div>
     </div>
   );
-}
+};
 
-export default Loader
+export default Loader;
